9e31f50a7d2d9077499597d1d0f79f985c1e7b58
sheptikhinva <sheptikhinv@gmail.com>
2023-08-22 15:33:58 +0500

    post updated

--- app/src/main/java/com/example/vkfuture/ui/view/News.kt
+++ app/src/main/java/com/example/vkfuture/ui/view/News.kt
@@ -36,6 +36,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -43,12 +44,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.InspectableModifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -152,19 +155,24 @@ fun Post(post: PostObj) {
 
 
             }
-            Text(post.text)
-            Row {
-                TextButton(onClick = { /*TODO*/ }) {
-                    Row {
-                        Icon(Icons.Outlined.FavoriteBorder, contentDescription = "Лайк", Modifier.size(24.dp))
-                        Text("100")
-                    }
-                }
-                //FilledIconButton("100", Icons.Outlined.MailOutline, onClick = { /*TODO*/ })
-                FilledIconButton(onClick = { /*TODO*/ }) {
-                    Icon(Icons.Outlined.Send, contentDescription = "Репост")
-                }
+            Text(post.text, modifier = Modifier.padding(start = 12.dp), color = MaterialTheme.colorScheme.onSurface, fontSize = 24.sp)
+            Row(Modifier.padding(12.dp)) {
+                TextIconButton("100", Icons.Outlined.FavoriteBorder, onClick = { /*TODO*/ })
+                Spacer(Modifier.width(8.dp))
+                TextIconButton("100", Icons.Outlined.MailOutline, onClick = { /*TODO*/ })
+                Spacer(Modifier.width(8.dp))
+                TextIconButton("100", Icons.Outlined.Send, onClick = { /*TODO*/ })
             }
+        }
+    }
+}
+
+@Composable
+fun TextIconButton(text: String, icon: ImageVector, modifier: Modifier = Modifier, onClick: () -> Unit){
+    Surface(color = MaterialTheme.colorScheme.primary, contentColor = MaterialTheme.colorScheme.onPrimary, modifier = Modifier.clickable(onClick = onClick), shape = RoundedCornerShape(100.dp)){
+        Row(Modifier.padding(4.dp), verticalAlignment = Alignment.CenterVertically){
+            Icon(imageVector = icon, contentDescription = text, Modifier.padding(4.dp))
+            Text(text, fontSize = 18.sp, modifier = Modifier.padding(end = 4.dp))
         }
     }
 }
