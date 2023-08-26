f61fc826a2b7da0a9608343bf5fc5a642f28ec9c
Georgy <chernihov.g@gmail.com>
2023-08-26 17:02:30 +0500

    AutActivity

---
+++ app/src/main/java/com/example/vkfuture/ui/view/AutActivity.kt
@@ -0,0 +1,42 @@
+package com.example.vkfuture.ui.view
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.example.vkfuture.data.model.modelnews.Token
+import com.vk.api.sdk.VK
+import com.vk.api.sdk.auth.VKAuthenticationResult
+import com.vk.api.sdk.auth.VKScope
+
+class AutActivity: ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val prefManager = PreferenceManager(this)
+        var token = prefManager.getData("access_token", "")
+        var user_id = prefManager.getData("user_id", "")
+
+        if (token == "") {
+            val authLauncher = VK.login(this) { result: VKAuthenticationResult ->
+                when (result) {
+                    is VKAuthenticationResult.Success -> {
+                        prefManager.saveData("access_token", result.token.accessToken)
+                        prefManager.saveData("user_id", result.token.userId.toString())
+                        token = result.token.accessToken
+                        user_id = result.token.userId.value.toString()
+                        Token.setToken(token, user_id)
+                        startActivity(Intent(this, MainActivity::class.java))
+                    }
+                    is VKAuthenticationResult.Failed -> {
+
+                    }
+                }
+            }
+            authLauncher.launch(arrayListOf(VKScope.WALL, VKScope.PHOTOS, VKScope.FRIENDS))
+        } else {
+            Token.setToken(token, user_id)
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}
